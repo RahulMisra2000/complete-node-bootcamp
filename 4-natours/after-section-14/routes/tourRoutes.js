a03c6bb4f35c633f075e1a9201c7b0d11354162a
RahulMisra2000 <RahulMisra2000@users.noreply.github.com>
2019-08-15 13:01:39 -0400

    Router Comments

--- 4-natours/after-section-14/routes/tourRoutes.js
+++ 4-natours/after-section-14/routes/tourRoutes.js
@@ -3,27 +3,34 @@ const tourController = require('./../controllers/tourController');
 const authController = require('./../controllers/authController');
 const reviewRouter = require('./../routes/reviewRoutes');
 
-const router = express.Router();
+// ********************** Create a router ***********************************************************************************
+const router = express.Router();  
 
 // router.param('id', tourController.checkID);
 
 // POST /tour/234fad4/reviews
 // GET /tour/234fad4/reviews
 
+// ************** This is a nested Router ****************************************************************************************
 router.use('/:tourId/reviews', reviewRouter);
+// *******************************************************************************************************************************
 
+
+// **************** Conventional  usage router.route(  ).httpVerb(Middlewares,..,Handler);   *************************************
+//                                                                              the Handler is also a middleware, the difference
+//                                                                              is that it sends response to the client     res.
+// *******************************************************************************************************************************
 router
   .route('/top-5-cheap')
   .get(tourController.aliasTopTours, tourController.getAllTours);
 
-router.route('/tour-stats').get(tourController.getTourStats);
+router
+  .route('/tour-stats')
+  .get(tourController.getTourStats);
+
 router
   .route('/monthly-plan/:year')
-  .get(
-    authController.protect,
-    authController.restrictTo('admin', 'lead-guide', 'guide'),
-    tourController.getMonthlyPlan
-  );
+  .get(authController.protect, authController.restrictTo('admin', 'lead-guide', 'guide'), tourController.getMonthlyPlan);
 
 router
   .route('/tours-within/:distance/center/:latlng/unit/:unit')
@@ -31,31 +38,20 @@ router
 // /tours-within?distance=233&center=-40,45&unit=mi
 // /tours-within/233/center/-40,45/unit/mi
 
-router.route('/distances/:latlng/unit/:unit').get(tourController.getDistances);
+router
+  .route('/distances/:latlng/unit/:unit')
+  .get(tourController.getDistances);
 
 router
   .route('/')
   .get(tourController.getAllTours)
-  .post(
-    authController.protect,
-    authController.restrictTo('admin', 'lead-guide'),
-    tourController.createTour
-  );
+  .post(authController.protect, authController.restrictTo('admin', 'lead-guide'), tourController.createTour);
 
 router
   .route('/:id')
   .get(tourController.getTour)
-  .patch(
-    authController.protect,
-    authController.restrictTo('admin', 'lead-guide'),
-    tourController.uploadTourImages,
-    tourController.resizeTourImages,
-    tourController.updateTour
+  .patch(authController.protect,authController.restrictTo('admin', 'lead-guide'), tourController.uploadTourImages, tourController.resizeTourImages, tourController.updateTour
   )
-  .delete(
-    authController.protect,
-    authController.restrictTo('admin', 'lead-guide'),
-    tourController.deleteTour
-  );
+  .delete(authController.protect, authController.restrictTo('admin', 'lead-guide'), tourController.deleteTour);
 
 module.exports = router;
