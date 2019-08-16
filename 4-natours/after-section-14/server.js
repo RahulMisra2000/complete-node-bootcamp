867169d81233883f5a877afc0f4cdd3d23b7a296
RahulMisra2000 <RahulMisra2000@users.noreply.github.com>
2019-08-16 13:28:42 -0400

    Using mongoose

--- 4-natours/after-section-14/server.js
+++ 4-natours/after-section-14/server.js
@@ -10,17 +10,9 @@ process.on('uncaughtException', err => {
 dotenv.config({ path: './config.env' });
 const app = require('./app');
 
-const DB = process.env.DATABASE.replace(
-  '<PASSWORD>',
-  process.env.DATABASE_PASSWORD
-);
+const DB = process.env.DATABASE.replace('<PASSWORD>', process.env.DATABASE_PASSWORD);
 
-mongoose
-  .connect(DB, {
-    useNewUrlParser: true,
-    useCreateIndex: true,
-    useFindAndModify: false
-  })
+mongoose.connect(DB, {useNewUrlParser: true, useCreateIndex: true, useFindAndModify: false})
   .then(() => console.log('DB connection successful!'));
 
 const port = process.env.PORT || 3000;
