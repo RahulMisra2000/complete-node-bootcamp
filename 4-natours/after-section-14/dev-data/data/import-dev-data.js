2ec7d566b68e1a53e6d033fbee55d0d008384105
RahulMisra2000 <RahulMisra2000@users.noreply.github.com>
2019-08-16 21:46:26 -0400

    Nice script for importing data

--- 4-natours/after-section-14/dev-data/data/import-dev-data.js
+++ 4-natours/after-section-14/dev-data/data/import-dev-data.js
@@ -7,25 +7,16 @@ const User = require('./../../models/userModel');
 
 dotenv.config({ path: './config.env' });
 
-const DB = process.env.DATABASE.replace(
-  '<PASSWORD>',
-  process.env.DATABASE_PASSWORD
-);
+const DB = process.env.DATABASE.replace('<PASSWORD>', process.env.DATABASE_PASSWORD);
 
 mongoose
-  .connect(DB, {
-    useNewUrlParser: true,
-    useCreateIndex: true,
-    useFindAndModify: false
-  })
+  .connect(DB, {useNewUrlParser: true,useCreateIndex: true,useFindAndModify: false})
   .then(() => console.log('DB connection successful!'));
 
 // READ JSON FILE
 const tours = JSON.parse(fs.readFileSync(`${__dirname}/tours.json`, 'utf-8'));
 const users = JSON.parse(fs.readFileSync(`${__dirname}/users.json`, 'utf-8'));
-const reviews = JSON.parse(
-  fs.readFileSync(`${__dirname}/reviews.json`, 'utf-8')
-);
+const reviews = JSON.parse(fs.readFileSync(`${__dirname}/reviews.json`, 'utf-8'));
 
 // IMPORT DATA INTO DB
 const importData = async () => {
@@ -37,7 +28,7 @@ const importData = async () => {
   } catch (err) {
     console.log(err);
   }
-  process.exit();
+  process.exit();           // *********************************
 };
 
 // DELETE ALL DATA FROM DB
