0c1028aa65b585f5d0820892a9ff817d283c9971
RahulMisra2000 <RahulMisra2000@users.noreply.github.com>
2019-08-29 23:48:32 -0400

    Handling Promise Rejections Centrally

--- 4-natours/after-section-14/server.js
+++ 4-natours/after-section-14/server.js
@@ -8,29 +8,42 @@ process.on('uncaughtException', err => {
 });
 
 dotenv.config({ path: './config.env' });
+
+// ***************** EXPRESS APP **************************************************************************
 const app = require('./app');
 
+
+
+// ***************** Database connection logic ************************************************************
 const DB = process.env.DATABASE.replace('<PASSWORD>', process.env.DATABASE_PASSWORD);
 
 mongoose.connect(DB, {useNewUrlParser: true, useCreateIndex: true, useFindAndModify: false})
   .then(() => console.log('DB connection successful!'));
+// ***************** Database connection logic ************************************************************
 
+
+// ***************** Start the Web Server that comes with Express *****************************************
 const port = process.env.PORT || 3000;
+// ********* We save the server so we can use it later ***
 const server = app.listen(port, () => {
-  console.log(`App running on port ${port}...`);
+    console.log(`App running on port ${port}...`);
 });
 
+
+
+// ********* All unhandled Promise Rejections ***************************************************************
 process.on('unhandledRejection', err => {
-  console.log('UNHANDLED REJECTION! 💥 Shutting down...');
-  console.log(err.name, err.message);
-  server.close(() => {
-    process.exit(1);
-  }); 
+    console.log('UNHANDLED REJECTION! 💥 Shutting down...');
+    console.log(err.name, err.message);
+    server.close(() => {                                    // Shutdown our Server **************************
+        process.exit(1);                                    // Now exit our node application ****************
+    }); 
 });
+
 
 process.on('SIGTERM', () => {
-  console.log('👋 SIGTERM RECEIVED. Shutting down gracefully');
-  server.close(() => {
-    console.log('💥 Process terminated!');
-  });
+    console.log('👋 SIGTERM RECEIVED. Shutting down gracefully');
+    server.close(() => {
+        console.log('💥 Process terminated!');
+    });
 });
