90461354f37dfd00023e16b704cd779844fc5bae
RahulMisra2000 <RahulMisra2000@users.noreply.github.com>
2019-08-29 23:57:17 -0400

    uncaughtException - should be the first thing coded

--- 4-natours/after-section-14/server.js
+++ 4-natours/after-section-14/server.js
@@ -1,11 +1,15 @@
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 
+// ********************************* This handler should be the FIRST thing *******************************
 process.on('uncaughtException', err => {
   console.log('UNCAUGHT EXCEPTION! 💥 Shutting down...');
   console.log(err.name, err.message);
   process.exit(1);
 });
+
+
+
 
 dotenv.config({ path: './config.env' });
 
