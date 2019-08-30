a373356ba3cea797400a61d416a2258a2c484543
RahulMisra2000 <RahulMisra2000@users.noreply.github.com>
2019-08-29 20:17:12 -0400

    my comments

--- 4-natours/after-section-14/utils/catchAsync.js
+++ 4-natours/after-section-14/utils/catchAsync.js
@@ -1,5 +1,6 @@
-module.exports = fn => {
+// Whoever calls this function will call it with a single parameter which is a function
+module.exports = (fn) => {              
   return (req, res, next) => {
-    fn(req, res, next).catch(next);
+    fn(req, res, next).catch(next);       // The function fn better return a promise
   };
 };
