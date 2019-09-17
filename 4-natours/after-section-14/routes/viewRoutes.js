5fb5c14052a8a7ee9445ff59ebdfbc3399f189b6
RahulMisra2000 <RahulMisra2000@users.noreply.github.com>
2019-09-17 00:35:00 -0400

    Commented

--- 4-natours/after-section-14/routes/viewRoutes.js
+++ 4-natours/after-section-14/routes/viewRoutes.js
@@ -6,18 +6,18 @@ const router = express.Router();
 
 router.use(viewsController.alerts);
 
+// ******** Difference between the two middlewares ****************************************************************************************
+// ******** authController.isLoggedIn    : This middleware just adds req.user if the cookie had a jwt token *******************************
+// ******** authController.protect       : This middleware short circuits and calls the global error handler if a valid jwt was not in the http request header or cookie ************************
 router.get('/', authController.isLoggedIn, viewsController.getOverview);
 
-router.get('/tour/:slug', authController.isLoggedIn, viewsController.getTour);
-router.get('/login', authController.isLoggedIn, viewsController.getLoginForm);
-router.get('/me', authController.protect, viewsController.getAccount);
 
-router.get('/my-tours', authController.protect, viewsController.getMyTours);
 
-router.post(
-  '/submit-user-data',
-  authController.protect,
-  viewsController.updateUserData
-);
+router.get('/tour/:slug',         authController.isLoggedIn, viewsController.getTour);
+router.get('/login',              authController.isLoggedIn, viewsController.getLoginForm);
+router.get('/me',                 authController.protect, viewsController.getAccount);
+router.get('/my-tours',           authController.protect, viewsController.getMyTours);
+
+router.post('/submit-user-data',  authController.protect,  viewsController.updateUserData);
 
 module.exports = router;
