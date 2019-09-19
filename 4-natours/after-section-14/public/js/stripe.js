6c1bb8071e9497ac5cd46639036ea51162ca8400
RahulMisra2000 <RahulMisra2000@users.noreply.github.com>
2019-09-19 14:09:40 -0400

    Client code processing for payment

--- 4-natours/after-section-14/public/js/stripe.js
+++ 4-natours/after-section-14/public/js/stripe.js
@@ -1,18 +1,15 @@
-/* eslint-disable */
+/* eslint-disable */    //* Disabling because we don't want eslint running on client js
 import axios from 'axios';
 import { showAlert } from './alerts';
 const stripe = Stripe('pk_test_BUkd0ZXAj6m0q0jMyRgBxNns00PPtgvjjr');
 
 export const bookTour = async tourId => {
   try {
-    // 1) Get checkout session from API
+    //********************** Call our API and have it call Stripe's API to create a checkout session and get it back to us
     const session = await axios(`/api/v1/bookings/checkout-session/${tourId}`);
-    // console.log(session);
-
-    // 2) Create checkout form + chanre credit card
-    await stripe.redirectToCheckout({
-      sessionId: session.data.session.id
-    });
+ 
+    //********************** Call Stripe's API and have them show THEIR payment screen to ask use for payment details and authorization
+                    await stripe.redirectToCheckout({sessionId: session.data.session.id});
   } catch (err) {
     console.log(err);
     showAlert('error', err);
