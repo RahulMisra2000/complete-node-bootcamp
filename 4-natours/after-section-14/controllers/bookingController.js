a3ed57d5c85cbfad17b8bf0811a61beec8c97f5f
RahulMisra2000 <RahulMisra2000@users.noreply.github.com>
2019-09-20 07:11:55 -0400

    HEROKU and WEBHOOK

--- 4-natours/after-section-14/controllers/bookingController.js
+++ 4-natours/after-section-14/controllers/bookingController.js
@@ -51,24 +51,40 @@ const createBookingCheckout = async session => {
   await Booking.create({ tour, user, price });
 };
 
+
+/* *********** I am reproducing a part of the code from app.js here for REFERENCE ONLY
+               Author configured HEROKU to send a post request to /webhook-checkout when a CC charge goes through
+               so, any res.send etc from any middleware at this route will be sent BACK to HEROKU and NOT TO OUR application
+app.post(
+  '/webhook-checkout',
+  bodyParser.raw({ type: 'application/json' }),
+  bookingController.webhookCheckout           //***** this last middleware is coded below
+* **************//
+
 exports.webhookCheckout = (req, res, next) => {
-  const signature = req.headers['stripe-signature'];
+  
+  //*** Think of this as getting the signature from the header and making sure all is KOSHER and getting hold of the event object *****
+      const signature = req.headers['stripe-signature'];
+      let event;
+      try {
+        event = stripe.webhooks.constructEvent(
+          req.body,
+          signature,
+          process.env.STRIPE_WEBHOOK_SECRET
+        );
+      } catch (err) {
+        return res.status(400).send(`Webhook error: ${err.message}`);       // Sent to HEROKU
+      }
+  //*** Think of this as getting the signature from the header and making sure all is KOSHER and getting hold of the event object *****
+  
+  //*** We can configure HEROKU to send us all kinds of events as and when they happen and we can specify to HEROKU
+  //    to have all of them come to a single endpoint like eg /webhook-checkout and that is WHY here we check the event type so 
+  //    accordingly we can process 
+    if (event.type === 'checkout.session.completed')
+      createBookingCheckout(event.data.object);    // Add a record in mongoDB using information from the session object 
+                                                   // that we had Stripe create at the time the CC transaction was being done
 
-  let event;
-  try {
-    event = stripe.webhooks.constructEvent(
-      req.body,
-      signature,
-      process.env.STRIPE_WEBHOOK_SECRET
-    );
-  } catch (err) {
-    return res.status(400).send(`Webhook error: ${err.message}`);
-  }
-
-  if (event.type === 'checkout.session.completed')
-    createBookingCheckout(event.data.object);
-
-  res.status(200).json({ received: true });
+    res.status(200).json({ received: true });                              // Sent to HEROKU
 };
 
 exports.createBooking = factory.createOne(Booking);
